282d69030ea665ae13175b8dc58935c0a7ab1b6d
Connor The Maker <connortmaker@gmail.com>
2026-01-21 15:38:02 -0500

    FEATURE: Copied gcode file now is copied the number of times that the user puts into the variable

--- Gcode Automation Script/copy script.py
+++ Gcode Automation Script/copy script.py
@@ -5,19 +5,32 @@ import os #The Library for manuevering in the File systems
 file_path = r"C:\Users\morei\OneDrive\Documents\GitHub\CPT-101\Gcode Automation Script\3DBenchy_PLA_37m39s.gcode"
 # Amount of Copies of the Gcode
 copies = 2
+# Output of the Operation:
+# output_path = r"C:\Users\morei\OneDrive\Documents\GitHub\CPT-101\Gcode Automation Script\Outputs" #Old Output path
 
 # Do the thing
 try:
-    # opens the file and reads it 
+    # The Paths for the new file with the copies
+    folder_path = os.path.dirname(file_path)
+    output_path = os.path.join(folder_path, f"{file_path}_QTY_{copies}.gcode")
+
+
+    # opens the soruce file and reads it 
     with open(file_path, "r", encoding="utf-8") as file:
         #creates a variable with the contents of the file
         contents = file.read()
-        #using the pyerpclip library, it performs a copy function
-        pyperclip.copy(contents)
-        print("File has been copied")
+    
+    # Writes the number of copies of the gcode file:
+    with open(output_path, "w", encoding="utf-8") as out_file:
+        for i in range(copies):
+            out_file.write(contents) #Writes the amount of copies of the gcode into 1 file
+            out_file.write("\n") #Adds a blank line at the end of the file
+            print(f"wrote {i+1} of {copies}")
 
+    print(f"Saved to {output_path}")
+    
 # When it breaks
 except FileNotFoundError:
     print("can't find it")
 except Exception as e:
-    print("it broke because {e}")
+    print(f"it broke because {e}")
