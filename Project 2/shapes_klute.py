1f90aead8b67f51a365c733b0bd2f4653a6eb169
Connor The Maker <connortmaker@gmail.com>
2026-04-15 19:45:01 -0400

    Finished All Major Functions of the Program, still need the Main Loop

--- Project 2/shapes_klute.py
+++ Project 2/shapes_klute.py
@@ -11,8 +11,11 @@ def get_menu_input(is_new_run):
     # Create a Menu for the USER to interact with
     if is_new_run:
         print("\nShape Drawing Program")
+
+    # inital variable to check against
+    choice = 0
     
-    while True:
+    while choice != 1 or choice != 2 or choice != 3 or choice !=4:
         print("\n This program can draw three different shapes\n")
         print("1. Draw Circle")
         print("2. Draw Square")
@@ -27,6 +30,7 @@ def get_menu_input(is_new_run):
                 print("\nNot Valid Menu Option")
         except ValueError:
             print("\nNot a Valid Menu Option")
+            choice = 0 # Resets the choice is an invalid number
 
 
 def get_color():
@@ -35,12 +39,69 @@ def get_color():
     color = input("What Color would you like the shape to be? (red, orange, purple)").lower().strip()
     while color != "red" or color != "orange" or color != "purple":
         print("Not a Valid Option")
-        color = input("What Color would you like the shape to be? (red, orange, purple)").lower().strip()
+        color = input("What Color would you like the shape to be? (red, orange, purple)").lower().strip() 
     return color
 
 def get_coords(axis_name, min_val, max_val):
+    # Prompts User for the x and y coordinates of the start point and ensures it fits in the given window
+    
+    # Initial Values to Check Against
+    valid_input = False
+    val = 0
+
+    while not valid_input:
+        try:
+            val = int(input(f"What is the {axis_name} value for your desired start location? ({min_val} to {max_val}) "))
+            if min_val <= val <= max_val:
+                valid_input = True: # True if the Coordinates are within Bounds
+            else:
+                print("Not Valid Input \n")
+        except ValueError:
+            print("Not Valid Input")
+    return val
 
 def get_size(choice, min_val, max_val):
+    # Prompts the User for the Radius or Side Length Based on the Shape input
+    if choice == 1:
+        print("Input a Radius to draw the Circle\n")
+        prompt = f"What is the radius? ({min_val} to {max_val}) "
+        reprompt = "What is the radius? "
+        error_msg = f"Please select a value between {min_val} and {max_val}.\n"
+    elif choice == 2:
+        print("Input a Length of the Side to draw the Square\n")
+        prompt = f"What is the length of the side? ({min_val} to {max_val}) "
+        reprompt = "What is the length of the side? "
+        error_msg = f"Please select a value between {min_val} and {max_val}.\n"
+    elif choice == 3:
+        print("Input a Length of the Side to draw the Triangle\n")
+        prompt = f"What is the length of the side? ({min_val} to {max_val}) "
+        reprompt = "What is the length of the side? "
+        error_msg = f"Please select a value between 0 and {max_val}.\n"
+
+        # Initial variables to check against
+        valid_input = False
+        size = 0
+        first_prompt = True
+
+        # Loop To Check if the inputs are valid
+
+        while not valid_input:
+            try:
+                if first_prompt:
+                    size = int(input(prompt))
+                    first_prompt = False
+                else: 
+                    size = int(input(reprompt))
+                
+                if min_val <= size <= max_val:
+                    valid_input = True
+                else:
+                    print(error_msg)
+                    first_prompt = False
+            except ValueError:
+                print(error_msg)
+                first_prompt = False
+    return size
 
 def draw_circle(x, y, radius, color):
     # Setup the Pen
