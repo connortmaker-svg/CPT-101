6e954addb764939b859b0fc9f7f463f66061e06d
Connor The Maker <connortmaker@gmail.com>
2026-05-04 21:00:09 -0400

    yaaay it works; created dice program that counts and analyzes the dice rolls given a user input

--- Project 3/dice_klute.py
+++ Project 3/dice_klute.py
@@ -25,10 +25,58 @@ def get_rolls():
             print("INVALID INPUT")
     return rolls
 
-def write_file(file, num_roll):
+def increment_roll_count(current_count):
+    # increments the number by 1
+    return current_count + 1
 
-def read_file(file, total_roll):
+def stats_dispaly(number, count, total):
+    # Calculates the percentage of the number passed through
+    if total > 0:
+        percentage = (count / total) * 100
+    else:
+        percentage = 0.0
+    print(f"Number {number}: Count = {count}, Percentage = {percentage:.1f}%")
+
+def write_file(file, num_roll):
+    # Writes the data of the rolls to an external .txt file
+    print(f"\nWriting results to {file}.....")
+    output = open(file, "w")
+
+    for i in range(num_roll):
+        result = random.randint(1,6)
+        output.write(str(result) + "\n")
+    output.close()
+
+def get_roll_count(name, target):
+    # Reads the file and counts how many times each "target" (or the number of the die roll) appears
+    count = 0 
+    try:
+        in_file = open(name, "r")
+        for line in in_file:
+            if int(line.strip()) == target:
+                count = increment_roll_count(count)
+        in_file.close()
+    except FileNotFoundError:
+        print("FILE NOT FOUND")
+    return count
 
 def main():
+    SIDES = 6
+    file = "dice_results.txt"
+
+    #Get user input
+    total_rolls = get_rolls()
+
+    # Make the File
+    write_file(file, total_rolls)
+
+    print(f"Analying {total_rolls} rolls...\n")
+
+    # Loop for the specific number of sides to the die
+    for current_side in range(1, SIDES + 1):
+        # Count how many times a specific side appears in the file
+        side_count = get_roll_count(file, current_side)
+        # pass the count to the display and percent calc function
+        stats_dispaly(current_side, side_count, total_rolls)
 
 main()
