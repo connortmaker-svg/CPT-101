850763ad0eede09ab1de57ea473d20ca8a8d996e
Connor The Maker <connortmaker@gmail.com>
2026-05-11 02:57:21 -0400

    Created Case swapper for the M13 lab

--- M13/flipper_klute.py
+++ M13/flipper_klute.py
@@ -4,3 +4,14 @@
 # M13 Lab - Working with Strings
 # Initial Finder
 # ----------------------------------
+
+# Get Inital User Input
+sent = input(" Enter a Sentence:  ")
+
+# user swacase function to switch the casing of the sentance
+flip = sent.swapcase()
+
+# Print Output
+print()
+print(f"Original Sentence: {sent}")
+print(f"Flipped Sentance: {flip}")
