d389e826b31d4078ccdd5fc5b387459621019f21
Connor The Maker <connortmaker@gmail.com>
2026-05-12 10:16:05 -0400

    Created framework for the program

---
+++ FINAL PROJECT/game_klute.py
@@ -0,0 +1,55 @@
+# ----------------------------------
+# Connor Klute
+# CPT 101 - Shaffer
+# Final Project
+# Adventure Quest
+# ----------------------------------
+
+import random # For Random Functionality
+
+def starting_player_config():
+    return 1
+
+def explore_scene():
+    # Scenario 1
+    # Scenario 2
+    # Scenario 3
+    # Scenario 4
+    # Scenario 5
+    # Scenario 6
+    # Scenario 7 
+    # Scenario 8
+    # Scenario 9
+    # Scenario 10
+    return 1
+
+def encounters():
+    # Encounter 1
+    # Encounter 2
+    # Encounter 3
+    # Encounter 4
+    # Encounter 5
+    # Encounter 6
+    # Encounter 7
+    return 1
+
+def display_stats():
+    return 1
+
+def final_boss_option():
+    return 1
+
+def exit():
+    return 1
+
+def menu_option():
+    return 1
+
+def start_menu():
+    return 1
+
+def player_inventory():
+    return 1
+
+
+
