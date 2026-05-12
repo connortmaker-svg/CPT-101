1ac9d615dd33f7f87c70bb55124793c568fa6c48
Connor The Maker <connortmaker@gmail.com>
2026-05-12 10:32:02 -0400

    created the menu functionality

--- FINAL PROJECT/game_klute.py
+++ FINAL PROJECT/game_klute.py
@@ -7,10 +7,35 @@
 
 import random # For Random Functionality
 
-def starting_player_config():
-    return 1
+def display_menu():
+    # Prints out the Menu for the User to Interact with.
+    print("1 | Explore your Surroundings")
+    print("2 | Have an Encounter")
+    print("3 | View Inventory and Player Stats")
+    print("4 | Fight the Dungeon Boss")
+    print("5 | Exit the Game")
 
-def explore_scene():
+def menu_select():
+    # Initalization Variables
+    v_input = False
+    option = 0
+    while not v_input:
+        try:
+            # Inital User Input
+            option = int(input("Enter a Menua Option between 1 and 5:"))
+            display_menu()
+            # Range Validation
+            if option >=1 and option <=5:
+                v_input = True
+            else:
+                print("Must be between 1 and 5")
+        # Error Handling
+        except ValueError:
+            print("Must be a valid integer")
+    # Output the users Option
+    return option
+
+def explore_scene(health, gold):
     # Scenario 1
     # Scenario 2
     # Scenario 3
@@ -21,9 +46,9 @@ def explore_scene():
     # Scenario 8
     # Scenario 9
     # Scenario 10
-    return 1
+    return health, gold
 
-def encounters():
+def encounters(health, gold, inventory):
     # Encounter 1
     # Encounter 2
     # Encounter 3
@@ -31,25 +56,28 @@ def encounters():
     # Encounter 5
     # Encounter 6
     # Encounter 7
+    return health, gold, inventory
+
+def display_stats(health, gold, inventory):
+    return health, gold, inventory
+
+def display_inventory(inventory):
+    return list
+
+def final_boss_option(health, gold, inventory):
     return 1
 
-def display_stats():
-    return 1
+def main():
+    gold = 20
+    health = 10
+    inventory = []
 
-def final_boss_option():
-    return 1
+    game_over = False
 
-def exit():
-    return 1
+    if game_over == False:
+        # Do everything
+        return 1
+    else:
+        exit():
 
-def menu_option():
-    return 1
-
-def start_menu():
-    return 1
-
-def player_inventory():
-    return 1
-
-
-
+main()
